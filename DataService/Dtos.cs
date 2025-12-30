7e38508fe6e1533c1e7e214c6996499c758a64eb
Reymon Reyes <elmoe01@gmail.com>
2025-12-30 16:13:41 +0800

    Add project files.

---
+++ DataService/Dtos.cs
@@ -0,0 +1,32 @@
+﻿namespace BlazorDashboards.Services.Dtos
+{
+    public class MonthlyData
+    {
+        private List<string> monthLabels = new List<string>
+        {
+            "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+        };
+
+        public int Month { get; set; }
+        public string MonthName { get { return monthLabels[Month - 1]; } }
+        public decimal Total { get; set; }
+    }
+
+    public class ProductCategorySales
+    {
+        public string CategoryName { get; set; }
+        public decimal Total { get; set; }
+    }
+
+    public class ProductSales
+    {
+        public string ProductName { get; set; }
+        public decimal Total { get; set; }
+    }
+
+    public class CustomerSales
+    {
+        public string CustomerName { get; set; }
+        public decimal Total { get; set; }
+    }
+}
---
+++ DataService/NorthwindContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorDashboards.Data
+{
+    public class NorthwindContext : DbContext
+    {
+        public string DbPath;
+
+        public NorthwindContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(System.IO.Path.GetDirectoryName(Environment.ProcessPath), "northwind.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={DbPath}");
+        }
+    }
+}
